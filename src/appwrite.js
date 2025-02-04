49a94233212539a0ef2942e0f71f45eeeb5109c2
Mr-Irtaza <Mr.Irtaza.2005@gmail.com>
2025-02-05 01:54:14 +0500

    Updated Readme

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import {Client, Databases, ID, Query} from "appwrite";
+import { Client, Databases, ID, Query } from 'appwrite'
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
@@ -11,43 +11,42 @@ const client = new Client()
 const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
-    //1. Use Appwrite SDK to Check if the searchTerm already exist in the database
+    // 1. Use Appwrite SDK to check if the search term exists in the database
     try {
-        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID, [
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
             Query.equal('searchTerm', searchTerm),
         ])
 
-        //2. If it does, update the count
-        if (result.documents.length > 0) {
+        // 2. If it does, update the count
+        if(result.documents.length > 0) {
             const doc = result.documents[0];
 
             await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
                 count: doc.count + 1,
             })
-            //3. If it doesn't create a new document with the search term and count as 1
+            // 3. If it doesn't, create a new document with the search term and count as 1
         } else {
             await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
                 searchTerm,
                 count: 1,
                 movie_id: movie.id,
-                poster_url:    `https://image.tmdb.org/t/p/w500@{movie.poster_path}`,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
             })
-
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
 export const getTrendingMovies = async () => {
     try {
-        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
             Query.limit(5),
-        Query.orderDesc("count")
-
+            Query.orderDesc("count")
         ])
-    return result.documents;
+
+        return result.documents;
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
